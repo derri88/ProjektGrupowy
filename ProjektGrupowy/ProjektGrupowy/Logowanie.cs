35060e1c638a199665eab0abd16da64e16ee68d6
Dominik <derecki.dominik@gmail.com>
2012-12-05 22:18:04 +0100

    Logowanie
    Dla testów działa już logowanie.
    Trzeba jakąś oficjalną bazę danych wkońcu ogarnąć, bo inaczej nikt się
    nie zaloguje!

--- ProjektGrupowy/ProjektGrupowy/Logowanie.cs
+++ ProjektGrupowy/ProjektGrupowy/Logowanie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,12 +23,81 @@ namespace ProjektGrupowy
             Program.Start1.Show();
         }
 
+        public int DaneLogowania() //Z powodu braku oficjalnej bazy, zrobiłem bazę o nazwie probra, gdzie za login sluzy imie a za hasło nazwisko.
+        {
+            int ID, count;
+            string Imie, Nazwisko;
+            string ConnectionString = "Server=(localdb)\\Projects;Database=Proba;Trusted_Connection=True;";
+            string IleRekordow = "SELECT COUNT(*) " +
+                                    "FROM Pracownik " +
+                                    "WHERE Imie = '" + LoginBox.Text + "'" +
+                                    "AND Nazwisko = '" + HasloBox.Text + "'";
+
+            string CheckUser = "SELECT Id, Imie, Nazwisko " +
+                                    "FROM Pracownik " +
+                                    "WHERE Imie = '" + LoginBox.Text + "'" +
+                                    "AND Nazwisko = '" + HasloBox.Text + "'";
+
+            SqlConnection Conn = new SqlConnection(ConnectionString);
+            Conn.Open();
+            SqlCommand DataCmd = new SqlCommand(CheckUser, Conn);
+            SqlCommand IleRek = new SqlCommand(IleRekordow, Conn);
+
+            count = (Int32)IleRek.ExecuteScalar(); //Spradzam czy zapytanie zwraca 1 rekord (login i hasło muszą byc unikalne! Więc zaptanie musi zwracać jeden rekord)
+
+            SqlDataReader Data = DataCmd.ExecuteReader();
+            if (count == 1 && Data.Read())
+            {
+                Imie = Data[1].ToString();
+                Nazwisko = Data[2].ToString();
+                ID = Data.GetInt32(0);
+                return ID;
+            }
+            else return 0;
+        }
+        /*Inny sposob na logowanie (uboższy bo nie zwraca ID klienta, co nie pozwala rozkminic kto jest zalogowany - wiec nie do wykorzystania, ale narazie niech lezy)
+        public bool UserCheck()
+        {
+            string ConnectionString = "Server=(localdb)\\Projects;Database=Proba;Trusted_Connection=True;";
+            SqlConnection Conn = new SqlConnection(ConnectionString);
+            Conn.Open();
+            CheckUser = "SELECT Id, Imie, Nazwisko " +
+                         "FROM Pracownik " +
+                         "WHERE Imie = '" + LoginBox.Text + "'" +
+                         "AND Nazwisko = '" + HasloBox.Text + "'";
+
+            SqlCommand DataCmd = new SqlCommand(CheckUser, Conn);
+            SqlDataReader Data = DataCmd.ExecuteReader();
+            if (Data.HasRows)
+            {
+                Data.Close();
+                Conn.Close();
+                return true;
+            }
+            else
+            {
+                Data.Close();
+                Conn.Close();
+                return false;
+
+            }
+
+
+        }
+        */
         private void LoginOK_Click(object sender, EventArgs e)
         {
-            Panel Panel = new Panel();
-            Panel.Show();
-            Program.Start1.Hide();
-            this.Close();
+            if (DaneLogowania() != 0)
+            {
+                Panel Panel = new Panel();
+                Panel.Show();
+                Program.Start1.Hide();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Błędny Login i/lub Hasło");
+            }
         }
 
         private void LoginAnuluj_Click(object sender, EventArgs e)
